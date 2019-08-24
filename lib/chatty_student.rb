55caa1af51fb0d11d161909075e60e9b1afd5856
Patrick Harrison <jahman@bellatlantic.net>
2019-08-24 12:23:50 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,14 +1,4 @@
 class ChattyStudent < Student
- def hello
-   
-   puts "Hey there! I'm so excited to learn stuff."
-   
-   end
-   
-    def raise_hand
-      
-      puts "Pick me!"
-      
-    end
+
     
 end
